49707f9f27b32a76076244bb2f422480b0aad617
Diego Azulay <dsazulay@gmail.com>
2019-02-17 17:09:04 -0400

    Vue data is now a function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 var app = new Vue({
     el: "#app",
-    data: {
-        name: myData.bio.name,
-        occupation: myData.bio.role,
-        location: myData.bio.contacts.location,
-        email: myData.bio.contacts.email,
-        phone: myData.bio.contacts.mobile,
-        skills: myData.bio.skills,
-        languages: myData.bio.languages,
-        schools: myData.education.schools,
-        jobs: myData.work.jobs
+    data() {
+        return {
+            name: myData.bio.name,
+            occupation: myData.bio.role,
+            location: myData.bio.contacts.location,
+            email: myData.bio.contacts.email,
+            phone: myData.bio.contacts.mobile,
+            skills: myData.bio.skills,
+            languages: myData.bio.languages,
+            schools: myData.education.schools,
+            jobs: myData.work.jobs
+        }
     },
     methods: {
         showInfo: function(event) {
